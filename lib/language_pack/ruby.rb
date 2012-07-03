de7204b6fd30e4323847e9003c2cef6626135652
Trey Bean <trey@12spokes.com>
2012-07-03 11:12:41 -0600

    Remove some debug output.

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -400,9 +400,6 @@ ERROR
         # codon since it uses bundler.
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:$CPATH CPPATH=#{yaml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\" CFLAGS=\"-fopenmp -I#{imagemagick_include}\" CPPFLAGS=\"-fopenmp -I#{imagemagick_include}\" LDFLAGS=\"-L#{imagemagick_lib} -Wl,-R#{imagemagick_lib} -L#{app_imagemagick_lib} -Wl,-R#{app_imagemagick_lib}\" LIBS=\"-L#{imagemagick_lib} -L#{app_imagemagick_lib}\""
         puts "Running: #{bundle_command}"
-        puts "with env_vars: #{env_vars}"
-        puts "PATH: #{`env echo $PATH`}"
-        puts "IM lib contents: #{`env ls #{pwd}/vendor/ImageMagick-6.7.8-0/lib`}"
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
 
       end
